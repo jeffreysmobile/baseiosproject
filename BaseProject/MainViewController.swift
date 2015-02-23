8f7ba1a18ef07d2143e45244ac2e68da44ecef69
Bogdan V <bogdanv@Georges-iMac-2.local>
2015-02-23 14:29:34 +0200

    create project - initial commit

---
+++ BaseProject/MainViewController.swift
@@ -0,0 +1,26 @@
+//
+//  MainViewController.swift
+//  BaseProject
+//
+//  Created by George on 23/02/15.
+//  Copyright (c) 2015 George. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
